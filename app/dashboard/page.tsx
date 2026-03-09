27034281362c316cbb98bf8f8243544a910eb79a
armir.shehaj <armir.shehaj@gmail.com>
2026-03-09 12:10:52 +0100

    fix: prevent stale dashboard data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import Dashboard from "@/components/app/dashboard/Dashboard"
 import { supabaseAdmin } from "@/lib/supabase"
 
+export const dynamic = "force-dynamic"
+
 export default async function Leads() {
     const {data: leads} = await supabaseAdmin.from('leads').select('*')
     
